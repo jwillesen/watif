c8b6596022c88c532faaa3ce2d2767d0ddf67214
jwillesen <git@wizardwell.net>
2017-07-13 22:01:11 -0600

    debug adapter should not swallow errors

--- packages/watif-jailed/src/debug-adapter.js
+++ packages/watif-jailed/src/debug-adapter.js
@@ -1,14 +1,27 @@
 import {Engine, Universe} from 'watif-interpreter'
-import InterfaceProvider from './interface-provider'
 
 export default class DebugAdapter {
   constructor () {
     this.universe = new Universe()
     this.engine = new Engine(this.universe)
-    this.provider = new InterfaceProvider(this.engine, this)
   }
 
-  setInterface (methods) {
-    Object.assign(this, methods)
+  echo = (message, cb) => {
+    cb(message)
+  }
+
+  loadStory = (storyCode, cb) => {
+    this.engine.loadStory(storyCode)
+    this.sendDisplayData(cb)
+  }
+
+  executeVerb = (verbInvokation, cb) => {
+    this.engine.executeVerb(verbInvokation)
+    this.sendDisplayData(cb)
+  }
+
+  replaceState = (newState, cb) => {
+    this.engine.replaceState(newState)
+    this.sendDisplayData(cb)
   }
 }
